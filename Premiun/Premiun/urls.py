6b2c7b65899fc9c9495b030aa52c72f55c38c76c
jairojg <jairojg@uninorte.edu.co>
2021-10-19 19:00:27 +0000

    fix: improved forms for login

--- Premiun/Premiun/urls.py
+++ Premiun/Premiun/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('landing/',saludo),
     path('login/',views.login),
-    path('registry/',views.registry)
+    path('registry/',views.registry),
+    path('session/',views.session)
     
 ]
--- Premiun/login/views.py
+++ Premiun/login/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from login.models import Cliente
 import pathlib
 # Create your views here.
 
@@ -10,9 +11,22 @@ location = pathlib.Path().resolve()
 #views
 def login(request):
 
-    return render(request, "{}/Premiun/login/templates/login.html".format(location)) 
-   
+    correo   = request.GET["email"]
+    password = request.GET["password"]
 
+    usuarios  = Cliente.objects.filter(email=correo).filter(password=password)
+    
+
+    return render(request, "{}/Premiun/login/templates/login.html".format(location)) 
 
 def registry(request):
-    return render(request,"{}/Premiun/login/templates/registry.html".format(registry))
+    return render(request,"{}/Premiun/login/templates/registry.html".format(location))
+
+def session(request):
+
+    nombre   = request.GET["nombre"]
+    apellido = reques.GET["apellido"]
+    correo   = request.GET["email"]
+    password = request.GET["password"]
+
+    return render(request,"{}/Premiun/login/templates/session.html".format(location))
