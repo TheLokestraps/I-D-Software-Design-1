133573fd9059cd7c02ec48b4ac7ce4dac5b6eb19
kangcheng322 <55262361+kangcheng322@users.noreply.github.com>
2021-11-13 16:06:29 -0500

    feat: :zap: Change sign in design

--- Formulario/views.py
+++ Formulario/views.py
@@ -12,7 +12,7 @@ def ofrecer(request):
    return render(request, "contact.html", None)
 
 def aviso(request):
-   print(request.POST["Nombre"], request.POST["Email"], request.POST["Indicativo"], request.POST["Telefono"], request.POST["DocumentoIdentidad"])
+  #print(request.POST["Nombre"], request.POST["Email"], request.POST["Indicativo"], request.POST["Telefono"], request.POST["DocumentoIdentidad"])
    x = Formulario(nombreCompleto=request.POST["Nombre"], email=request.POST["Email"], indicativo= request.POST["Indicativo"], telefono= request.POST["Telefono"], docIdentidad= request.POST["DocumentoIdentidad"])
    x.save()
    mensaje = request.POST["Nombre"]
