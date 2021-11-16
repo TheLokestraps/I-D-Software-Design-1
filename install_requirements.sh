19b1856459e94fc14646a749d1e30d618ed58db7
jairojg <jairojg@uninorte.edu.co>
2021-11-15 23:21:32 -0500

    feat: added needed dependencies of django libs

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 sudo apt install python3-pip
 pip install Django==2.2.3
+pip install django-allauth
+pip install django-mptt
 django-admin startproject Premiun
 python3 Premiun/manage.py migrate
